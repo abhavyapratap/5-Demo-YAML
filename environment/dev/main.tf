2a738116e349b7d600167c65c6f1cd0b083ca728
Bhavya pratap singh <bhavyasingh801@gmail.com>
2025-08-05 10:39:10 +0530

    Adding Resources as per Ticket203

---
+++ environment/dev/main.tf
@@ -0,0 +1,23 @@
+module "vnet" {
+  source              = "../../modules/azurerm_vnet"
+  vnet_name           = "V-Net"
+  location            = "centralindia"
+  resource_group_name = "rg-demon-slayer"
+  address_space       = ["10.0.0.0/16"]
+}
+
+module "subnet" {
+  source               = "../../modules/azurerm_subnet"
+  subnet_name          = "frontend-subnet"
+  resource_group_name  = "rg-demon-slayer"
+  virtual_network_name = "V-Net"
+  address_prefixes     = ["10.0.0.0/24"]
+}
+
+module "subnet1" {
+  source               = "../../modules/azurerm_subnet"
+  subnet_name          = "backend-subnet"
+  resource_group_name  = "rg-demon-slayer"
+  virtual_network_name = "V-Net"
+  address_prefixes     = ["10.0.1.0/24"]
+}
